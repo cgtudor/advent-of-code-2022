c0c1c6492c579d7aa089b4a4572600aa7e9aeb40
Cristian Tudor <cristian.tudor@red-gate.com>
2022-12-06 16:17:06 +0000

    Day 1 completed

--- advent-of-code-2022/day-1/Program.cs
+++ advent-of-code-2022/day-1/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 var lines = File.ReadAllLines(@"C:\\source\\advent-of-code-2022\\advent-of-code-2022\\day-1\\day1_input.txt");
+var allElvesCalories = new List<int>();
+
 var maxCalories = 0;
 var currentElfCalories = 0;
 
@@ -9,12 +11,16 @@ foreach(var line in lines) {
     //Console.WriteLine(line);
     if (line.Length == 0)
     {
-        maxCalories = currentElfCalories > maxCalories ? currentElfCalories : maxCalories;
+        allElvesCalories.Add(currentElfCalories);
         currentElfCalories = 0;
         continue;
     }
     currentElfCalories += int.Parse(line);
 }
+allElvesCalories.Add(currentElfCalories);
+var descendingElvesCalories = allElvesCalories.OrderByDescending(x => x).ToList();
 
-maxCalories = currentElfCalories > maxCalories ? currentElfCalories : maxCalories;
-Console.WriteLine("Most calories carried: " + maxCalories);
+Console.WriteLine("Most calories carried: " + descendingElvesCalories[0]);
+
+var sumOfTopThree = descendingElvesCalories[0] + descendingElvesCalories[1] + descendingElvesCalories[2];
+Console.WriteLine("Sum of top 3 calories carried: " + sumOfTopThree);
