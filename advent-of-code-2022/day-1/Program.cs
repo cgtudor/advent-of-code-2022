9bff4b6b9be13511143b739b76a3b08d4399f6ff
Cristian Tudor <cristian.tudor@red-gate.com>
2022-12-06 16:07:54 +0000

    Day 1 Part 1 puzzle initial solution

--- advent-of-code-2022/day-1/Program.cs
+++ advent-of-code-2022/day-1/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var lines = File.ReadAllLines(@"C:\\source\\advent-of-code-2022\\advent-of-code-2022\\day-1\\day1_input.txt");
+var maxCalories = 0;
+var currentElfCalories = 0;
+
+//Console.WriteLine("Contents of day1 input: ");
+foreach(var line in lines) {
+    //Console.WriteLine(line);
+    if (line.Length == 0)
+    {
+        maxCalories = currentElfCalories > maxCalories ? currentElfCalories : maxCalories;
+        currentElfCalories = 0;
+        continue;
+    }
+    currentElfCalories += int.Parse(line);
+}
+
+maxCalories = currentElfCalories > maxCalories ? currentElfCalories : maxCalories;
+Console.WriteLine("Most calories carried: " + maxCalories);
